6ca32d91d874a3d1ed0ac7a1e743c77277b1b58b
Davidsjoborg <154664943+Davidsjoborg@users.noreply.github.com>
2026-05-23 15:57:12 +0200

    Added code for left and right

--- main.py
+++ main.py
@@ -83,10 +83,70 @@ def DownButtonClick():
             Win()
 
 def LeftButtonClick():
-    GAME_ZIP64.run_motor(100)
+    global X_C, Y_C, X_TC, Y_TC , StepCount
+    GAME_ZIP64.run_motor(50)
+    #led.unplot(X_C, Y_C)
+    X_C -= 1
+    StepCount += 1
+
+    #IF target over laps with player
+    if X_C == X_TC and Y_C == Y_TC:
+        Win()
+    else:
+        #Makes you go to the other side
+        if X_C < 0:
+            X_C = 7
+        
+        #led.plot(X_C, Y_C)
+        #led.unplot(X_TC, Y_TC)
+        
+        X_TC = X_TC + randint(-1, 1)
+        Y_TC = Y_TC + randint(-1, 1)
+        
+        targetStopp()
+
+        display.clear()
+        display.set_matrix_color(X_C, Y_C, GAME_ZIP64.colors(ZipLedColors.GREEN))
+        display.set_matrix_color(X_TC, Y_TC, GAME_ZIP64.colors(ZipLedColors.RED))
+        display.show()
+
+        #led.plot(X_TC, Y_TC)
+        #IF target over laps with player
+        if X_C == X_TC and Y_C == Y_TC:
+            Win()
 
 def RightButtonClick():
-    GAME_ZIP64.run_motor(100)
+    global X_C, Y_C, X_TC, Y_TC , StepCount
+    GAME_ZIP64.run_motor(50)
+    #led.unplot(X_C, Y_C)
+    X_C += 1
+    StepCount += 1
+
+    #IF target over laps with player
+    if X_C == X_TC and Y_C == Y_TC:
+        Win()
+    else:
+        #Makes you go to the other side
+        if X_C > 7:
+            X_C = 0
+        
+        #led.plot(X_C, Y_C)
+        #led.unplot(X_TC, Y_TC)
+        
+        X_TC = X_TC + randint(-1, 1)
+        Y_TC = Y_TC + randint(-1, 1)
+        
+        targetStopp()
+
+        display.clear()
+        display.set_matrix_color(X_C, Y_C, GAME_ZIP64.colors(ZipLedColors.GREEN))
+        display.set_matrix_color(X_TC, Y_TC, GAME_ZIP64.colors(ZipLedColors.RED))
+        display.show()
+
+        #led.plot(X_TC, Y_TC)
+        #IF target over laps with player
+        if X_C == X_TC and Y_C == Y_TC:
+            Win()
 
 GAME_ZIP64.on_button_press(GAME_ZIP64.ZIP64ButtonPins.UP,
     GAME_ZIP64.ZIP64ButtonEvents.CLICK,
--- main.ts
+++ main.ts
@@ -93,10 +93,70 @@ GAME_ZIP64.onButtonPress(GAME_ZIP64.ZIP64ButtonPins.Down, GAME_ZIP64.ZIP64Button
     
 })
 GAME_ZIP64.onButtonPress(GAME_ZIP64.ZIP64ButtonPins.Left, GAME_ZIP64.ZIP64ButtonEvents.Click, function LeftButtonClick() {
-    GAME_ZIP64.runMotor(100)
+    
+    GAME_ZIP64.runMotor(50)
+    // led.unplot(X_C, Y_C)
+    X_C -= 1
+    StepCount += 1
+    // IF target over laps with player
+    if (X_C == X_TC && Y_C == Y_TC) {
+        Win()
+    } else {
+        // Makes you go to the other side
+        if (X_C < 0) {
+            X_C = 7
+        }
+        
+        // led.plot(X_C, Y_C)
+        // led.unplot(X_TC, Y_TC)
+        X_TC = X_TC + randint(-1, 1)
+        Y_TC = Y_TC + randint(-1, 1)
+        targetStopp()
+        display.clear()
+        display.setMatrixColor(X_C, Y_C, GAME_ZIP64.colors(ZipLedColors.Green))
+        display.setMatrixColor(X_TC, Y_TC, GAME_ZIP64.colors(ZipLedColors.Red))
+        display.show()
+        // led.plot(X_TC, Y_TC)
+        // IF target over laps with player
+        if (X_C == X_TC && Y_C == Y_TC) {
+            Win()
+        }
+        
+    }
+    
 })
 GAME_ZIP64.onButtonPress(GAME_ZIP64.ZIP64ButtonPins.Right, GAME_ZIP64.ZIP64ButtonEvents.Click, function RightButtonClick() {
-    GAME_ZIP64.runMotor(100)
+    
+    GAME_ZIP64.runMotor(50)
+    // led.unplot(X_C, Y_C)
+    X_C += 1
+    StepCount += 1
+    // IF target over laps with player
+    if (X_C == X_TC && Y_C == Y_TC) {
+        Win()
+    } else {
+        // Makes you go to the other side
+        if (X_C > 7) {
+            X_C = 0
+        }
+        
+        // led.plot(X_C, Y_C)
+        // led.unplot(X_TC, Y_TC)
+        X_TC = X_TC + randint(-1, 1)
+        Y_TC = Y_TC + randint(-1, 1)
+        targetStopp()
+        display.clear()
+        display.setMatrixColor(X_C, Y_C, GAME_ZIP64.colors(ZipLedColors.Green))
+        display.setMatrixColor(X_TC, Y_TC, GAME_ZIP64.colors(ZipLedColors.Red))
+        display.show()
+        // led.plot(X_TC, Y_TC)
+        // IF target over laps with player
+        if (X_C == X_TC && Y_C == Y_TC) {
+            Win()
+        }
+        
+    }
+    
 })
 let display = GAME_ZIP64.createZIP64Display()
 display.setBrightness(10)
